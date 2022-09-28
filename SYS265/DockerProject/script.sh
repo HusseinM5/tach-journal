944156dde556972d7be84d69c0d7e67fa6662aee
HusseinM5 <90108325+HusseinM5@users.noreply.github.com>
2022-09-28 19:36:28 -0400

    Update script.sh

--- SYS265/DockerProject/script.sh
+++ SYS265/DockerProject/script.sh
@@ -4,9 +4,10 @@ echo ""
 ## DOWNLOADING AND CREATING DOCKER COMPOSE FILES ##
 mkdir /home/mohammed/docker-db
 wget https://raw.githubusercontent.com/HusseinM5/tech-journal/main/SYS265/DockerProject/docker-compose-db.yml -O /home/mohammed/docker-db/docker-compose.yml
-wget https://raw.githubusercontent.com/HusseinM5/tech-journal/main/SYS265/DockerProject/Dockerfile -O /home/mohammed/docker-db/Dockerfile
+
 mkdir /home/mohammed/docker-web
 wget https://raw.githubusercontent.com/HusseinM5/tech-journal/main/SYS265/DockerProject/docker-compose-web.yml -O /home/mohammed/docker-web/docker-compose.yml
+wget https://raw.githubusercontent.com/HusseinM5/tech-journal/main/SYS265/DockerProject/Dockerfile -O /home/mohammed/docker-web/Dockerfile
 wget https://raw.githubusercontent.com/HusseinM5/tech-journal/main/SYS265/DockerProject/index.php -O /home/mohammed/docker-web/index.php
 wget https://raw.githubusercontent.com/HusseinM5/tech-journal/main/SYS265/DockerProject/server.php -O /home/mohammed/docker-web/server.php
 wget https://raw.githubusercontent.com/HusseinM5/tech-journal/main/SYS265/DockerProject/errors.php -O /home/mohammed/docker-web/errors.php
