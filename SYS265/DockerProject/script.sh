e52949187275ed9acb9ebf3335ee1375b08f062c
HusseinM5 <90108325+HusseinM5@users.noreply.github.com>
2022-09-28 19:11:43 -0400

    Update script.sh

--- SYS265/DockerProject/script.sh
+++ SYS265/DockerProject/script.sh
@@ -6,13 +6,13 @@ echo $'\e[1;32m'"Downloading files..."$'\e[0m'
 echo ""
 ## DOWNLOADING AND CREATING DOCKER COMPOSE FILES ##
 mkdir /home/$WOW/docker-db
-wget https://raw.githubusercontent.com/HusseinM5/tech-journal/main/SYS265/DockerProject/docker-compose-db.yml -o /home/$WOW/docker-db/docker-compose.yml
-wget https://raw.githubusercontent.com/HusseinM5/tech-journal/main/SYS265/DockerProject/Dockerfile -o /home/$WOW/docker-db/Dockerfile
+wget https://raw.githubusercontent.com/HusseinM5/tech-journal/main/SYS265/DockerProject/docker-compose-db.yml -O /home/$WOW/docker-db/docker-compose.yml
+wget https://raw.githubusercontent.com/HusseinM5/tech-journal/main/SYS265/DockerProject/Dockerfile -O /home/$WOW/docker-db/Dockerfile
 mkdir /home/$WOW/docker-web
-wget https://raw.githubusercontent.com/HusseinM5/tech-journal/main/SYS265/D$ockerProject/docker-compose-web.yml -o /home/$WOW/docker-web/docker-compose.yml
-wget https://raw.githubusercontent.com/HusseinM5/tech-journal/main/SYS265/DockerProject/index.php -o /home/$WOW/docker-web/index.php
-wget https://raw.githubusercontent.com/HusseinM5/tech-journal/main/SYS265/DockerProject/server.php -o /home/$WOW/docker-web/server.php
-wget https://raw.githubusercontent.com/HusseinM5/tech-journal/main/SYS265/DockerProject/errors.php -o /home/$WOW/docker-web/errors.php
+wget https://raw.githubusercontent.com/HusseinM5/tech-journal/main/SYS265/D$ockerProject/docker-compose-web.yml -O /home/$WOW/docker-web/docker-compose.yml
+wget https://raw.githubusercontent.com/HusseinM5/tech-journal/main/SYS265/DockerProject/index.php -O /home/$WOW/docker-web/index.php
+wget https://raw.githubusercontent.com/HusseinM5/tech-journal/main/SYS265/DockerProject/server.php -O /home/$WOW/docker-web/server.php
+wget https://raw.githubusercontent.com/HusseinM5/tech-journal/main/SYS265/DockerProject/errors.php -O /home/$WOW/docker-web/errors.php
 
 echo ""
 echo $'\e[1;32m'"Starting docker!"$'\e[0m'
